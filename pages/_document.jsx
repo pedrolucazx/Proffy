6610f5ee73b2fe815501ae55f8b98669787dd501
forge1010 <ale.silva1120@gmail.com>
2021-09-25 19:39:25 -0300

    template

---
+++ pages/_document.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {Html, Head , Main, NextScript} from "next/document";
+
+export default class Mydocument extends Document {
+    render(){
+        return(
+         <Html lang="pt-BR">
+             <Head>
+             <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;700&display=swap" rel="stylesheet"/>
+
+    
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;700&family=Poppins:wght@300;400;700&display=swap" rel="stylesheet"/>
+             </Head>
+             <body>
+                 <Main/>
+                 <NextScript />
+             </body>
+         </Html>
+        );
+    }
+}
