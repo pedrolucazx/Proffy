21e155a25f08b3612cd9e53fc87900e2593fbecc
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-09-26 18:49:19 -0300

    feat: styles form page

--- pages/FormProffy.jsx
+++ pages/FormProffy.jsx
@@ -15,18 +15,78 @@ export default function FormProffy() {
           <img src="/Proffy.svg" alt="" />
         </div>
 
-        <div className={styles.headerText}>
-          <h1>Que incrível que você <br /> quer dar aulas.</h1>
-          <span> O primeiro passo, é preencher esse <br /> formulário de inscrição</span>
-        </div>
       </header>
       <main>
         <section className={styles.containerForm}>
+
+          <div className={styles.headerText}>
+            <h1>Que incrível que você <br /> quer dar aulas.</h1>
+            <span> O primeiro passo, é preencher esse <br /> formulário de inscrição</span>
+          </div>
+
           <form action="" >
-            <h2>Seus dados</h2>
-            <hr />
+
+            <div className={styles.data}>
+              <h2>Seus dados</h2>
+              <hr />
+              <label>Nome completo</label>
+              <input type="text" id="name" />
+              <label>Link da sua foto (comece com //http)</label>
+              <input type="url" id="photo" />
+              <label>Whatsapp</label>
+              <input type="tel" id="whatsapp" />
+              <label htmlFor="">Biografia</label>
+              <textarea id="bio"></textarea>
+            </div>
+
+            <div className={styles.class}>
+              <h2>Sobre a aula</h2>
+              <hr />
+              <label>Matéria</label>
+              <select id="materia">
+                <option value="" selected disabled>Selecione qual você quer ensinar</option>
+                <option value="artes">Artes</option>
+                <option value="biologia">Biologia</option>
+                <option value="ciencias">Ciências</option>
+                <option value="educacao_fisica">Educação Física</option>
+                <option value="fisica">Física</option>
+                <option value="geografia">Geografia</option>
+                <option value="historia">História</option>
+                <option value="matematica">Matemática</option>
+                <option value="portugues">Português</option>
+                <option value="quimica">Química</option>
+              </select>
+              <label> Custo da sua hora por aula (em R$)</label>
+              <input type="number" />
+            </div>
+
+            <div className={styles.schedules}>
+              <h2>Horários disponíveis</h2>
+              <h3>+ Novo horário</h3>
+              <hr />
+              <label>Dia da Semana</label>
+              <div className={styles.week}>
+                <select>
+                  <option value="" selected disabled> Selecione o dia</option>
+                  <option value="segunda">Segunda</option>
+                  <option value="terça">Terça</option>
+                  <option value="quarta">Quarta</option>
+                  <option value="quinta">Quinta</option>
+                  <option value="sexta">Sexta</option>
+                </select>
+                <input type="time" id="das"/>
+                <input type="time" id="ate"/>
+              </div>
+            </div>
+
+            <div className={styles.submit}>
+              <img src="/Atenção.svg" alt="" />
+              <span>Importante! <br /> Preencha todos os dados</span>
+              <button type="submit">Salvar cadastro</button>
+            </div>
             
           </form>
+
         </section>
       </main>
     </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,38 +6,31 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-    
-    <Head> <title>PROFFY | HOME</title> </Head>
-    <main className={styles.home}>
-
-      <div className={styles.container}>
-
-        <div className={styles.logo}>
-          <img src="/Proffy.svg" alt="" />
-          <span>Sua plataforma de<br /> estudos online</span>
-        </div>
-
-        <div className={styles.buttons}>
-          <button>
-            <img src="/Estudar.svg" alt="" />
-            <h3>Estudar</h3>
-          </button>
-          <Link href="/FormProffy" passHref>
-            <button >
-              <img src="/Dar aulas.svg" alt="" />
-              <h3>Dar aulas</h3>
+      <Head> <title>PROFFY | HOME</title> </Head>
+      <main className={styles.home}>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <img src="/Proffy.svg" alt="" />
+            <span>Sua plataforma de<br /> estudos online</span>
+          </div>
+          <div className={styles.buttons}>
+            <button>
+              <img src="/Estudar.svg" alt="" />
+              <h3>Estudar</h3>
             </button>
-          </Link>
+            <Link href="/FormProffy" passHref>
+              <button >
+                <img src="/Dar aulas.svg" alt="" />
+                <h3>Dar aulas</h3>
+              </button>
+            </Link>
+          </div>
         </div>
-
-      </div>
-
-      <div className={styles.ilusta}>
-        <img src="/Ilustra.svg" alt="" />
-        <span>Total de 285 conexões já realizadas</span>
-      </div>
-
-    </main>
+        <div className={styles.ilusta}>
+          <img src="/Ilustra.svg" alt="" />
+          <span>Total de 285 conexões já realizadas</span>
+        </div>
+      </main>
     </>
   )
 }
