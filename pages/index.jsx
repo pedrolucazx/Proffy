ac69315bc4f4777e37a54df69679006e530446ec
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-09-26 00:07:27 -0300

    feat: create home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
-   <main className={styles.home}>
-      <h1>hello world</h1>
-   </main>
+    <main className={styles.home}>
+
+      <div className={styles.container}>
+
+        <div className={styles.logo}>
+          <img src="/Proffy.svg" alt="" />
+          <span>Sua plataforma de<br /> estudos online</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button>
+            <img src="/Estudar.svg" alt="" />
+            <h3>Estudar</h3>
+          </button>
+          <button>
+            <img src="/Dar aulas.svg" alt="" />
+            <h3>Dar aulas</h3>
+          </button>
+        </div>
+
+      </div>
+
+      <div className={styles.ilusta}>
+        <img src="/Ilustra.svg" alt="" />
+        <span>Total de 285 conexões já realizadas</span>
+      </div>
+
+    </main>
   )
 }
